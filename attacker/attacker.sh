d66f90dea24fcfc2a71a1aac018c64af637566b2
juruizra <82108239+juruizra@users.noreply.github.com>
2025-03-03 19:33:40 -0500

    Fix: added missing results.txt file in server

--- attacker/attacker.sh
+++ attacker/attacker.sh
@@ -10,13 +10,13 @@ FORWARD_PID=$!
 # Set up trap to terminate the forwarding process when the script exits
 trap "echo 'Attacker: Terminating forwarding process...'; kill $FORWARD_PID 2>/dev/null" EXIT
 
-# Capture traffic on both networks without entering promiscuous mode for a limited time
+# Capture traffic on both networks 
 echo "Attacker: Starting packet capture..."
 timeout 30s tcpdump -p -i any -w /app/capture.pcap -v
 
 echo "Attacker: Captured traffic saved to /app/capture.pcap"
 
-# Instead of opening an interactive shell automatically,
-# leave the container running so you can exec into it later.
+
+# leave the container running to open a terminal later (trying to break the rsa).
 echo "Attacker: Capture complete. Container will remain running."
 tail -f /dev/null
